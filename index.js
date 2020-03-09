8be24f9b3666d76e6b60b4af6a40e35ec7890255
root <jesusjimenez_15@hotmail.com>
2020-03-09 19:10:37 +0000

    Se ha puesto el recurso /time tambien en el servidor comun

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var app = express(); //Por convenio se crea así la variable.
 
 app.use("/", express.static(__dirname+"/public/"));
 
+app.get("/time", (req, res) => {
+	console.log("Peticion enviada al servidor");
+	res.send("<html>"+Date()+"</html>");
+	
+} );
+
 app.listen(3000);
 
 console.log("Server ready fake");
