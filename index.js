fca36cdf2c4fb6b7a99017e7d3ed268dac4dfdad
root <jesusjimenez_15@hotmail.com>
2020-03-09 18:45:08 +0000

    Servidor montado con index.html funcionando

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var express = require('express');
 var app = express(); //Por convenio se crea así la variable.
 
 app.use("/", express.static(__dirname+"/public/"));
+
+app.listen(3000);
+
+console.log("Server ready fake");
+
