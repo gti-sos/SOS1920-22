1306ffdb7d3b68f45ea4fa2f151207ce14888845
root <casrodia@gmail.com>
2020-03-23 16:50:27 +0000

    Creacion API baloncesto

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ var nadadores = [
 	{ country: 'italy', year:'2009', yearofbirth:1987, position: 4, time: 21.08 },
 	{ country: 'united kingdom', year:'2018', yearofbirth:1994, position: 5, time: 21.11 },
 ];
+var baloncesto = [
+	{ country: 'serbia', year:'2016', points:66, threepoints: 4, rebounds: 33 },
+	{ country: 'spain', year:'2012', points:100, threepoints: 7, rebounds: 35 },
+	{ country: 'spain', year:'2008', points:107, threepoints: 8, rebounds: 37 },
+	{ country: 'italy', year:'2004', points:69, threepoints: 11, rebounds: 26 },
+	{ country: 'france', year:'2000', points:75, threepoints: 6, rebounds: 20 },
+];
 
 
 //Cargar datos iniciales - Fórmula 1 - loadInitialData.
@@ -127,7 +134,7 @@ app.get(baseURL+"/swim-stats/loadInitialData", (req,res) => {
 	console.log("Data sent: "+JSON.stringify(nadadores,null,2));
 })
 
-//RECURSOS GENERALES - API REST - Fórmula 1
+//RECURSOS GENERALES - API REST - Natacion
 
 app.get(baseURL + '/swim-stats', (request, response) => {
 	console.log(Date() + ' - GET /swim-stats');
@@ -192,9 +199,82 @@ app.put(baseURL + '/swimmers/:position', (request, response) => {
 		nadadores.push(request.body);
 		response.sendStatus(200);
 	}
-	
 });
 
+//Cargar datos iniciales - Baloncesto - loadInitialData.
+app.get(baseURL+"/formula-stats/loadInitialData", (req,res) => {
+	res.send(JSON.stringify(baloncesto,null,2));
+	console.log("Data sent: "+JSON.stringify(baloncesto,null,2));
+})
 
+
+
+//RECURSOS GENERALES - API REST - BALONCESTO
+
+app.get(baseURL + '/og-basket-stats', (request, response) => {
+	console.log(Date() + ' - GET /og-basket-stats');
+	response.send(baloncesto);
+});
+
+app.post(baseURL + '/og-basket-stats', (request, response) => {
+	console.log(Date() + ' - POST /og-basket-stats');
+	var aux = request.body;
+	baloncesto.push(aux);
+	response.sendStatus(201);
+});
+
+app.put(baseURL + '/og-basket-stats', (request, response) => {
+	console.log(Date() + ' - PUT /og-basket-stats');
+	response.send(405,);
+});
+
+app.delete(baseURL + '/og-basket-stats', (request, response) => {
+	console.log(Date() + ' - DELETE /og-basket-stats');
+	basket = []; 
+	response.sendStatus(200);
+});
+
+// RECURSOS ESPECÍFICOS - BALONCESTO
+
+app.get(baseURL + '/og-basket-stats/:year', (request, response) => {
+	var aux = request.params.year;
+	console.log(Date() + ' - GET /year - Recurso Específico' + aux);
+	var filtro = baloncesto.filter(n => n.year == aux);
+	response.send(filtro);
+
+});
+
+app.post(baseURL + '/og-basket-stats/:year', (request, response) => {
+	var aux = request.params.year;
+	console.log(Date() + ' - POST /year - Recurso Específico ' + aux);
+	response.send(405, "Method not allowed");
+	//response.send(405);
+});
+
+app.delete(baseURL + '/og-basket-stats/:year', (request, response) => {
+	var aux = request.params.year;
+	console.log(Date() + ' - DELETE /basket - Recurso Específico' + aux);
+	var filtro = baloncesto.filter(n => n.year != aux);
+	baloncesto = filtro;
+	response.sendStatus(200);
+
+});
+
+app.put(baseURL + '/og-basket-stats/:year', (request, response) => {
+	var aux = request.params.year;
+	var anyo = request.body.year;
+	if(aux != anyo){
+		response.sendStatus(409);
+		console.warn(Date()+ " Hacking Attempt !!!! ");
+	}	
+	else{
+		console.log(Date() + ' - PUT /year - Recurso Específico ' + aux);
+		var filtro = baloncesto.filter(n => n.year != aux); 
+		banloncesto = filtro; 
+		baloncesto.push(request.body);
+		response.sendStatus(200);
+	}
+	
+});
 
 
