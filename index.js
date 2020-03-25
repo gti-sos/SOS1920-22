6e3388327d3da867b6289f536025211cd5216c98
root <casrodia@gmail.com>
2020-03-25 14:44:47 +0000

    Lista baloncesto

--- index.js
+++ index.js
@@ -202,10 +202,9 @@ app.put(baseURL + '/swim-stats/:position', (request, response) => {
 });
 
 //Cargar datos iniciales - Baloncesto - loadInitialData.
-app.get(baseURL+"/formula-stats/loadInitialData", (req,res) => {
-	var basket = baloncesto;
-	res.send(JSON.stringify(basket,null,2));
-	console.log("Data sent: "+JSON.stringify(basket,null,2));
+app.get(baseURL+"/og-basket-stats/loadInitialData", (req,res) => {
+	res.send(JSON.stringify(baloncesto,null,2));
+	console.log("Data sent: "+JSON.stringify(baloncesto,null,2));
 })
 
 
