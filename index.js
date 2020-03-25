636cefbd02cb8c7ca8550386bcb5d7b9a8384642
juantot9 <totodile.alone9@gmail.com>
2020-03-25 18:56:06 +0000

    arreglamos problemas con los arrays con .slice()

--- index.js
+++ index.js
@@ -33,13 +33,7 @@ var pilotos = [
 	{ country: 'mexico', year:2016, totalpointnumber:101, pilotnumber: 2, victorynumber: 0 }
 ];
 
-var pilotosInitialData = [
-	{ country: 'germany', year:2019, totalpointnumber:568, pilotnumber: 5, victorynumber: 5 },
-	{ country: 'france', year:2019, totalpointnumber:32, pilotnumber: 3, victorynumber: 0  },
-	{ country: 'united kingdom', year:2014, totalpointnumber:475, pilotnumber: 4, victorynumber: 11 },
-	{ country: 'spain', year:2015, totalpointnumber:30, pilotnumber: 3, victorynumber: 0 },
-	{ country: 'mexico', year:2016, totalpointnumber:101, pilotnumber: 2, victorynumber: 0 }
-];
+var pilotosInitialData = pilotos.slice();
 
 var nadadores = [
 	{ country: 'brazil', year:2009, yearofbirth:1987, position: 1, time: 20.91 },
@@ -49,13 +43,7 @@ var nadadores = [
 	{ country: 'united kingdom', year:2018, yearofbirth:1994, position: 5, time: 21.11 },
 ];
 
-var nadadoresInitialData = [
-	{ country: 'brazil', year:2009, yearofbirth:1987, position: 1, time: 20.91 },
-	{ country: 'france', year:2009, yearofbirth:1981, position: 2, time: 20.94 },
-	{ country: 'korea', year:2019, yearofbirth:1996, position: 3, time: 21.04 },
-	{ country: 'italy', year:2009, yearofbirth:1987, position: 4, time: 21.08 },
-	{ country: 'united kingdom', year:2018, yearofbirth:1994, position: 5, time: 21.11 },
-];
+var nadadoresInitialData = nadadores.slice();
 
 var baloncesto = [
 	{ country: 'serbia', year:2016, points:66, threepoints: 4, rebounds: 33 },
@@ -64,13 +52,7 @@ var baloncesto = [
 	{ country: 'italy', year:2004, points:69, threepoints: 11, rebounds: 26 },
 	{ country: 'france', year:2000, points:75, threepoints: 6, rebounds: 20 },
 ];
-var baloncestoInitialData = [
-	{ country: 'serbia', year:2016, points:66, threepoints: 4, rebounds: 33 },
-	{ country: 'spain', year:2012, points:100, threepoints: 7, rebounds: 35 },
-	{ country: 'spain', year:2008, points:107, threepoints: 8, rebounds: 37 },
-	{ country: 'italy', year:2004, points:69, threepoints: 11, rebounds: 26 },
-	{ country: 'france', year:2000, points:75, threepoints: 6, rebounds: 20 },
-];
+var baloncestoInitialData = baloncesto.slice();
 
 
 //Cargar datos iniciales - Fórmula 1 - loadInitialData.
