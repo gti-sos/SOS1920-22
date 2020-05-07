8e220468abd2fb512e86f0810ac9d4bb217af670
jaarandat <jaarandat@somosgrupomas.com>
2020-05-07 21:45:51 +0200

    dejar bonita el home

--- src/front/Home.svelte
+++ src/front/Home.svelte
@@ -1,52 +1,59 @@
+<script>
+	import Box from './utilities/Box.svelte';
+</script>
 <main>
-	<article class="markdown-body entry-content" itemprop="text">
-		<ul>
-		<li><strong>Team</strong>
-		<ul>
-		<li><a href="https://github.com/jesjimmon">Jesús Jiménez Montero</a></li>
-		<li><a href="https://github.com/CastoRod">Casto Rodríguez Díaz</a></li>
-		<li><a href="https://github.com/juantot9">Juan Antonio Aranda Triana</a></li>
-		</ul>
-		</li>
-		<li><strong>Project description</strong>: The sources of information chosen are aimed at analyzing the relationship between the classification of Formula 1 drivers by nationality, teams that lost the final game in Olympic basketball history and the swimming world records.</li>
-		<li><strong>Repository</strong>: <a href="https://github.com/gti-sos/SOS1920-22">gti-sos/SOS1920-22</a></li>
-		<li><strong>URL</strong>: <a href="http://sos1920-22.herokuapp.com" rel="nofollow">http://sos1920-22.herokuapp.com</a></li>
-		<li><strong>Links de Postman</strong>:
-		<ul>
-		<li>(<a href="https://documenter.getpostman.com/view/10637349/SzYUXzfN" rel="nofollow">https://documenter.getpostman.com/view/10637349/SzYUXzfN</a>) (developed by <a href="https://github.com/jesjimmon">Jesús Jiménez Montero</a>)</li>
-		<li>(<a href="https://documenter.getpostman.com/view/10826099/SzYUXfdy" rel="nofollow">https://documenter.getpostman.com/view/10826099/SzYUXfdy</a>) (developed by <a href="https://github.com/CastoRod">Casto Rodríguez Díaz</a>)</li>
-		<li>(<a href="https://documenter.getpostman.com/view/10153510/SzYUXfiH" rel="nofollow">https://documenter.getpostman.com/view/10153510/SzYUXfiH</a>) (developed by <a href="https://github.com/juantot9">Juan Antonio Aranda Triana</a>)</li>
-		</ul>
-		</li>
-		<li><strong>APIs:</strong>
-		<ul>
-		<li>(<a href="http://sos1920-22.herokuapp.com/api/v1/formula-stats" rel="nofollow">http://sos1920-22.herokuapp.com/api/v1/formula-stats</a>) (developed by <a href="https://github.com/jesjimmon">Jesús Jiménez Montero</a>)</li>
-		<li>(<a href="https://sos1920-22.herokuapp.com/api/v1/og-basket-stats/" rel="nofollow">https://sos1920-22.herokuapp.com/api/v1/og-basket-stats/</a>) (developed by <a href="https://github.com/CastoRod">Casto Rodríguez Díaz</a>)</li>
-		<li>(<a href="https://sos1920-22.herokuapp.com/api/v1/swim-stats/" rel="nofollow">https://sos1920-22.herokuapp.com/api/v1/swim-stats/</a>) (developed by <a href="https://github.com/juantot9">Juan Antonio Aranda Triana</a>)</li>
-		</ul>
-		</li>
-		</ul>
-		<ul>
-			<li>
-				<strong>Botones para el frontend de nuestras APIs:</strong>
-			<ul>
-				<li>
-					<div class="div-home">
-						<button type="button"  class="btn btn-outline-success"  onclick="window.location.href='#/formula1API'"> API de Fórmula 1</button>
-					</div>
-				</li>
-				<li>
-					<div class="div-home">
-						<button type="button"  class="btn btn-outline-warning"  onclick="window.location.href='#/swimstatsAPI'"><i class='fas fa-leaf' ></i> API de Natación</button>
-					</div>
-				</li>
-				<li>
-					<div class="div-home">
-						<button type="button"  class="btn btn-outline-info"  onclick="window.location.href='#/basketAPI'"><i class='fas fa-leaf' ></i> API de Baloncesto</button>
-					</div>
-				</li>
-			</ul>
-			</li>
-		</ul>
-		</article>
+
+	<h2 style="text-align: center;margin-bottom: 40px;">Project description</h2>
+
+	<h6 style="text-align: center;margin-bottom: 40px;">The sources of information chosen are aimed at analyzing the relationship between the classification of Formula 1 drivers by nationality, teams that lost the final game in Olympic basketball history and the swimming world records.</h6>
+	<h2 style="text-align: center;margin-bottom: 40px;">Repository</h2>
+	<h4 style="text-align: center;margin-bottom: 40px;"><a href="https://github.com/gti-sos/SOS1920-22">gti-sos/SOS1920-22</a></h4>
+	<h2 style="text-align: center;margin-bottom: 40px;">URL</h2>
+	<h4 style="text-align: center;margin-bottom: 40px;"><a href="http://sos1920-22.herokuapp.com" rel="nofollow">http://sos1920-22.herokuapp.com</a></h4>
+	<h2 style="text-align: center;margin-bottom: 40px;">Team</h2>
+	<div style="display:flex;align-items:center;justify-content: center;margin-bottom: 40px;">
+	<Box>
+		<span slot="name">
+			Jesús Jiménez Montero
+		</span>
+		<span slot="pmaddress">
+			<a href="https://documenter.getpostman.com/view/10637349/SzYUXzfN" rel="nofollow">POSTMAN DE jesjimmon</a>
+		</span>
+		<span slot="apiurl">
+			<a href="http://sos1920-22.herokuapp.com/api/v2/formula-stats" rel="nofollow">APIv2 formula-stats</a>
+		</span>
+		<span slot="apibutton">
+			<button type="button"  class="btn btn-outline-success"  onclick="window.location.href='#/formula1API'"> API de Fórmula 1</button>
+		</span>
+	</Box>
+	<Box>
+		<span slot="name">
+			Casto Rodríguez	Díaz
+		</span>
+		<span slot="pmaddress">
+			<a href="https://documenter.getpostman.com/view/10826099/SzYUXfdy" rel="nofollow">POSTMAN DE casroddia</a>
+		</span>
+		<span slot="apiurl">
+			<a href="https://sos1920-22.herokuapp.com/api/v1/og-basket-stats/" rel="nofollow">APIv1 swim-stats</a>
+		</span>
+		<span slot="apibutton">
+			<button type="button"  class="btn btn-outline-info"  onclick="window.location.href='#/basketAPI'"><i class='fas fa-leaf' ></i> API de Baloncesto</button>
+		</span>
+	</Box>
+	<Box>
+		<span slot="name">
+			Juan Antonio Aranda Triana
+		</span>
+		<span slot="pmaddress">
+			<a href="https://documenter.getpostman.com/view/10153510/SzYUXfiH" rel="nofollow">POSTMAN DE juaaratri</a>
+		</span>
+		<span slot="apiurl">
+			<a href="http://sos1920-22.herokuapp.com/api/v1/swim-stats" rel="nofollow">APIv1 swim-stats</a>
+		</span>
+		<span slot="apibutton">
+			<button type="button"  class="btn btn-outline-danger"  onclick="window.location.href='#/swimstatsAPI'"><i class='fas fa-leaf' ></i> API de Natación</button>
+		</span>
+	</Box>
+
+	</div>
 </main>
---
+++ src/front/utilities/Box.svelte
@@ -0,0 +1,67 @@
+<style>
+	.box {
+		width: 400px;
+		border: 1px solid #aaa;
+		border-radius: 2px;
+		box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+		padding: 1em;
+	}
+
+	h3 {
+        text-align: center;
+		padding: 0 0 0.2em 0;
+		margin: 0 0 1em 0;
+		border-bottom: 1px solid #000000
+	}
+
+	.pmaddress {
+        position: relative;
+		padding: 0 0 0 1.5em;
+		background:  0 0 no-repeat;
+		background-size: 15px 15px;
+		margin: 0 0 0.5em 0;
+        line-height: 1.2;
+        left:85px;
+    }
+    .apiurl {
+        position: relative;
+        padding: 0 0 0 1.5em;
+		background:  0 0 no-repeat;
+		background-size: 20px 20px;
+		margin: 0 0 0.5em 0;
+        line-height: 1.2;
+        left:110px;
+    }
+    .apibutton {
+        text-align: center;
+    }
+
+	.pmaddress { background-image: url('https://www.postman.com/icons/icon-48x48.png?v=9a2f6f2a305951852f949fcec9a89efd') }
+	.apiurl   { background-image: url('https://upload.wikimedia.org/wikipedia/commons/8/87/VisualEditor_-_Icon_-_Code.svg') }
+	.missing { color: #999 }
+</style>
+
+<article class="box">
+	<h3>
+		<slot name="name">
+			<span class="missing">Unknown name</span>
+		</slot>
+	</h3>
+
+	<div class="pmaddress">
+		<slot name="pmaddress">
+			<span class="missing">Unknown address</span>
+		</slot>
+	</div>
+
+	<div class="apiurl">
+		<slot name="apiurl">
+			<span class="missing">Unknown email</span>
+		</slot>
+    </div>
+    <div class="apibutton">
+        <slot name="apibutton">
+            <button type="button"></button>
+        </slot>
+    </div>
+</article>
