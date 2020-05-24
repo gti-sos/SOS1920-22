c72aff227c7a07ead0c084f323d0c2953cc73fec
jesjimmon <jesusjimenez_15@hotmail.com>
2020-05-24 17:56:53 +0200

    added cors

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const port = process.env.PORT || 3000; //Anyadido para Heroku L05.
 const bodyParser = require('body-parser');
+const cors = require("cors");
 
 const app = express(); //Por convenio se crea así la variable.
+
+app.use(cors());
 
 const formula1API = require("./src/back/formula1API");
 const swimstatsAPI = require("./src/back/swimstatsAPI");
 const basketAPI = require("./src/back/basketAPI");
 app.use(bodyParser.json());
-app.use("/", express.static(__dirname+"/public/"));
+app.use("/", express.static(__dirname + "/public/"));
 
 //------- Llamada a API Fórmula 1 - Jesús Jiménez Montero -----
 formula1API(app);
@@ -17,16 +20,23 @@ swimstatsAPI(app);
 //=======LLamada a API Baloncesto - Casto Rodríguez Díaz=======\\
 basketAPI(app);
 
+//Proxy para Fórmula 1 - PRUEBAS
+
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
 
 //Primer entregable - Devuelve la hora actual del servidor.
 app.get("/time", (req, res) => {
 	console.log("Peticion enviada al servidor");
-	res.send("<html>"+Date()+"</html>");
-	
-} );
+	res.send("<html>" + Date() + "</html>");
+
+});
 
 app.listen(port, () => {
-	console.log("Server ready in port "+port);
+	console.log("Server ready in port " + port);
 });
 
 console.log("Starting server...");
