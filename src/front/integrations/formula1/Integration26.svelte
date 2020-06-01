672ec943029891d29533f2aff84c8ebc6fcd7dd1
jesjimmon <jesusjimenez_15@hotmail.com>
2020-06-01 21:23:34 +0200

    added 21 and 2 group with formula-stats

--- src/front/integrations/formula1/Integration26.svelte
+++ src/front/integrations/formula1/Integration26.svelte
@@ -4,7 +4,7 @@
 
     async function loadGraph() {
 
-        //FALTA CORS
+        //INTEGRADO
 
         const resDataFormula = await fetch("/api/v2/formula-stats");
         const resDataFoot = await fetch("http://sos1920-26.herokuapp.com/api/v3/goalscorers");
