7e110507005bfd502cf8b819c39ada57b40926fb
Casto <casrodia@gmail.com>
2020-06-01 16:59:26 +0200

    graph error

--- src/front/analytics/basketball/AwesomeBasketball.svelte
+++ src/front/analytics/basketball/AwesomeBasketball.svelte
@@ -6,13 +6,15 @@
         const resData = await fetch("/api/v1/og-basket-stats");
         let dataBasket = await resData.json();
         console.log("Base de datos:" + dataBasket);
+        let modifiedData = [["Paises (Anyo)", "Puntos", "Puntos de Triples", "Rebotes"]];
 
-        let allData = dataBasket.map((d) => {
-            let res = [d.country + "(" + d.year + ")", d.points, d.threepoints, d.rebounds];
+        let stats = dataBasket.map((d) => {
+            let res = [d.country + " (" + d.year + ")", d.points, d.threepoints, d.rebounds];
             return res;
         });
-        console.log(allData);
 
+        let allData = modifiedData.concat(stats); //Uno la lista de modified data con stats
+        console.log(allData);
         let numbers = dataBasket.map((d)=> {return d.points});
         let axisSize = Math.max.apply(null, numbers) + 10;
 
@@ -21,6 +23,7 @@
         var chart = bb.generate({
 
             data: {
+                x: "Paises (Anyo)",
                 columns: allData,
                 type: "radar",
                 labels: true
--- src/front/basketAPI/App.svelte
+++ src/front/basketAPI/App.svelte
@@ -3,8 +3,23 @@
 </script>
 
 <main>
-    <style>body{background:lightblue}</style>
+    <style>
+        body {
+            background: lightblue
+        }
+    </style>
     <h1 class="display-4" style="text-align:center;">API de Baloncesto Olimpiadas- SOS1920</h1>
-    <div align="center"><img src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Basketball_pictogram.svg" alt="LogoBasket" width="400" height="400"></div>
-    <BasketTable/>
+    <div align="center"><img src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Basketball_pictogram.svg"
+            alt="LogoBasket" width="400" height="400"></div>
+    <div style="margin-bottom: 25px">
+        <div>
+            <a href="https://sos1920-22.herokuapp.com/#/analytics/highcharts-og-basket-stats" rel="nofollow">Grafica
+                HighChart Baloncesto</a>
+        </div>
+        <div>
+            <a href="https://sos1920-22.herokuapp.com/#/analytics/awesome-og-basket-stats" rel="nofollow">Grafica
+                Awesome Chart Baloncesto</a>
+        </div>
+    </div>
+    <BasketTable />
 </main>
